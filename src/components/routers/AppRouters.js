b506fbc6a0c9603f76c2b4a628e2a94a16a43696
mvillagordo <m.villagordovera@gmail.com>
2020-08-26 06:40:50 +0200

    Change loading checking and is loggedIn

--- src/components/routers/AppRouters.js
+++ src/components/routers/AppRouters.js
@@ -1,9 +1,43 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import { AuthRouters } from './AuthRouters'
-import JournalScreen from '../journal/JournalScreen'
+import React, { useEffect, useState } from 'react';
+import { firebase } from '../firebase/firebaseConfig';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { AuthRouters } from './AuthRouters';
+import JournalScreen from '../journal/JournalScreen';
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
 
 export const AppRouters = () => {
+
+    const dispatch = useDispatch();
+
+    const [checking, setchecking] = useState(true);
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        
+        firebase.auth().onAuthStateChanged( (user) => {
+        
+            if( user?.uid ) {
+                dispatch( login( user.uid, user.displayName ) )
+                setIsLoggedIn(true);
+            } else {
+                setIsLoggedIn(false);
+            }
+        });
+
+        setchecking(false);
+
+    }, [dispatch, setchecking, setIsLoggedIn]);
+
+    
+    if( checking ) {
+        return (
+            <h1>Espere...</h1>
+        )
+    }
+
+
     return (
             <Router>
                 <div>

adada983215dd5a972d9e6e3488ec276fb376ca4
mvillagordo <m.villagordovera@gmail.com>
2020-10-09 17:39:25 +0200

    Refactorizamos la forma de obtener las notas con una acción

--- src/components/actions/notes.js
+++ src/components/actions/notes.js
@@ -1,4 +1,5 @@
 import { db } from "../firebase/firebaseConfig";
+import { loadNotes } from "../helpers/loadNotes";
 import { types } from "../types/types";
 
 export const startNewNote = () => {
@@ -28,6 +29,16 @@ export const activeNote = (id, note) => ({
         ...note
     }
 });
+
+export const startLoadingNotes = ( uid ) => {
+
+    return async ( dispatch ) => {
+    
+        const notes = await loadNotes( uid )
+            dispatch( setNotes( notes ) )
+    }
+
+}
 
 export const setNotes = ( notes ) => ({
 
--- src/components/routers/AppRouters.js
+++ src/components/routers/AppRouters.js
@@ -7,8 +7,7 @@ import { useDispatch } from 'react-redux';
 import { login } from '../actions/auth';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
-import { loadNotes } from '../helpers/loadNotes';
-import { setNotes } from '../actions/notes';
+import { startLoadingNotes } from '../actions/notes';
 
 export const AppRouters = () => {
 
@@ -25,9 +24,8 @@ export const AppRouters = () => {
             if( user?.uid ) {
                 dispatch( login( user.uid, user.displayName ) )
                 setIsLoggedIn(true);
-
-                const notes = await loadNotes( user.uid );
-                dispatch( setNotes( notes ) )
+                
+                dispatch( startLoadingNotes( user.uid ) );
 
             } else {
                 setIsLoggedIn(false);
